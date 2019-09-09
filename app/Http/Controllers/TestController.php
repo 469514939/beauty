2272afb4b856be86e62065e77e1e2ed3fa00acbe
469514939 <469514939@qq.com>
2019-09-10 00:14:26 +0800

    ok

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,15 +6,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Routing\Router;
 
+use App\Models\Products;
+
 class TestController extends Controller
 {
     
     //ceshi
     public function test()
     {
-        // $name = Route::getRoutes();
-        $name = app('router')->getRoutes();
-        dd($name);
+        // $name = app('router')->getRoutes();
+        // dd($name);
+        $list = Products::where('status',1)->paginate(15);
+        dd($list);
     }
 
 }
